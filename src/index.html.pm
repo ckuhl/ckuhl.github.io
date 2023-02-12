054d0e5ea1544b677a08e38487a4964d6a97dddf
Chris Kuhl <git@ckuhl.com>
2023-02-12 11:15:42 -0700

    fix: Remove txexpr from HTML

--- src/index.html.pm
+++ src/index.html.pm
@@ -2,27 +2,28 @@
 
 ◊(define-meta title "Chris' Web Log")
 
-◊(txexpr
-  (h1
+◊(h1
    "Under construction"
    (img #:src "./images/construction1.gif"))
-  (p "Welcome to the personal web log of Christian Kuhl.")
-  (hr)
-  (h2 "Things I've created")
-  (ul
+
+◊(p "Welcome to the personal web log of Christian Kuhl.")
+
+◊(hr)
+◊(h2 "Things I've created")
+◊(ul
    (li "a web log")
    (li
     (a #:href "/pollen-notes" "notes on using pollen!")
     (img #:src "./images/new2.gif"))
    (li "Code on " (a #:href "https://github.com/ckuhl" "GitHub")))
-  (hr)
-  (h2 "Stuff I like working on")
-  (ul
+◊(hr)
+◊(h2 "Stuff I like working on")
+◊(ul
    (li "Racket")
    (li "Advent of Code"))
-  (hr)
-  (h2 "Neat things")
-  (ul
+◊(hr)
+◊(h2 "Neat things")
+◊(ul
    (li "Words I had to look up")
    (li "Pictures of birds")
-   (li "Blog posts, sometimes, maybe")))
+   (li "Blog posts, sometimes, maybe"))
