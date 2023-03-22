d1748504cf7461b52d7bda1087a80df4364a4c1d
Chris Kuhl <git@ckuhl.com>
2023-03-21 19:34:52 -0600

    fix: Now with actually-valid tag

--- src/pollen.rkt
+++ src/pollen.rkt
@@ -3,10 +3,11 @@
 (require pollen/tag pollen/template pollen/core txexpr sugar/coerce)
 (provide (all-defined-out))
 
-; Alias the 'root element to actually be 'content
+; Alias the 'root element to actually be 'pollen
 ;; This is because 'root is not allowed to be a direct child of 'body
+;; Using a custom element is the best way to avoid causing any problems here
 (define (root . elements)
-   (txexpr 'content empty elements))
+   (txexpr 'pollen empty elements))
 
 ; Yoinked from:
 ;  https://github.com/rymaju/pollen-ryanjung.dev/blob/fc506d2292703b0086025cc72abe78cc3adad4e2/src/pollen.rkt#L15-L18
