725bd05dedbb2e81a5f32ec9a2f1f3dade75c249
Chris Kuhl <git@ckuhl.com>
2023-03-21 19:28:32 -0600

    fix: Change 'root to 'content

--- src/pollen.rkt
+++ src/pollen.rkt
@@ -3,6 +3,11 @@
 (require pollen/tag pollen/template pollen/core txexpr sugar/coerce)
 (provide (all-defined-out))
 
+; Alias the 'root element to actually be 'content
+;; This is because 'root is not allowed to be a direct child of 'body
+(define (root . elements)
+   (txexpr 'content empty elements))
+
 ; Yoinked from:
 ;  https://github.com/rymaju/pollen-ryanjung.dev/blob/fc506d2292703b0086025cc72abe78cc3adad4e2/src/pollen.rkt#L15-L18
 (define (link url text) `(a ((href ,url)) ,text))
