16039b63ccd67d2eb753341e0861057905a7cd20
Chris Kuhl <git@ckuhl.com>
2023-04-08 19:36:58 -0600

    content: Write first actual article
    Write rough-draft of intro article for web log.

--- src/pollen.rkt
+++ src/pollen.rkt
@@ -12,6 +12,7 @@
 ; Yoinked from:
 ;  https://github.com/rymaju/pollen-ryanjung.dev/blob/fc506d2292703b0086025cc72abe78cc3adad4e2/src/pollen.rkt#L15-L18
 (define (link url text) `(a ((href ,url)) ,text))
+
 (define (node->link node)
   (define node-string (->string node))
   (define link-name (or (select-from-metas 'title node) node-string))
--- src/weblog/web-site.html.pm
+++ src/weblog/web-site.html.pm
@@ -1,11 +1,134 @@
 #lang pollen
 
 ◊(define-meta title "Why a web log, why now?")
+◊(define-meta date "2023-04-08")
 
-◊h1{Why a web log, why now?}
+
+◊(h1 (select-from-metas 'title (current-metas)))
+◊(div (em (select-from-metas 'date (current-metas))))
 
 ◊p{
-TODO: Summarize bulleted notes into ✨ content ✨
+I have had fits and starts when it comes to writing a web log before.
+In the past, I have never stuck to it.
+This time, like every time, is different.
+}
+
+◊p{
+To that end, I am starting this web log by asking, well, ◊em{why}?
+}
+
+◊p{
+Instead of starting a web log with an inarticulate and uncertain answer, I want
+to start this log by thinking through a clear and concise answer. In doing so,
+I am also creating criteria for writing future content, giving myself a
+yard-stick with which to ask "does this fit" and editing accordingly.
+}
+
+◊p{
+So, why write a web log?
+}
+
+
+◊h2{It's fun!}
+
+◊p{
+It's not.
+}
+
+◊p{
+And yet, it is?
+}
+
+◊p{
+It's a little like telling the worst, most groan-worthy dad jokes (aside:
+Anyone can be a dad, it's simply a matter of making someone else face palm at
+the awfulness of your bad puns). Creating a web log, and really a whole entire
+web site, is something that people will stumble over, point at, and stare you
+in the eye and say "WHY ON EARTH WOULD YOU MAKE THAT." That's fun!
+}
+
+
+◊h2{It's a place to write}
+
+◊p{
+This one is true! While previously I have resorted to phone calls and/or
+texting a longstanding and longsuffering friend with a recent thought.
+}
+
+◊p{
+How-ever, why not summarize it and throw it out into the public where anyone can see and
+comment on it and judge you and post mean things on Hacker News?
+
+More seriously, the joy of the internet is that everybody is
+nobody and so there is really no fear in publishing something for my friends
+publicly. If you get it, you're my friend; and if you don't get it, well I
+didn't write it for you. Sorry.
+}
+
+◊p{
+There is a secondary utility in having a public writing place. In case you
+didn't know, I am kind of a big deal. Or at least my family thinks so.
+Which is really more stressful than actually being a big deal.
+Occasionally something pops up which breaches the realm of tech, and people
+start asking me questions (previously crypto, now ChatGPT).
+Having a place to publish content and refer to will likely result in more
+cogent answers than reloading ✨ work ✨ context into my brain when I am at a
+family gathering.
+}
+
+
+◊h2{Some weird sense of obligation}
+
+◊p{
+Not, like, some weird societal obligation. Please don't think I'm writing for
+you human reading the web log right now, that's weird. Rather I am writing a web
+log because that is what young stand-out software developers do, and I am
+certainly a young stand-out software developer, how-do-you-do.
+}
+
+◊p{
+Hey you, prospective employer, how many other applicants have web logs?
+}
+
+◊p{
+It's also a digital net-working sort-of web site.
+Like, if you find it, you can click through to other pages to email me if
+you’re so inspired.
+You could do it right now if you want. It’s free. You probably won’t go to
+spam, even!
+}
+
+◊p{
+It is, in a sense, away to build solidarity with people who also have tech-ish
+blog-ish web sites. As it turns out, writing and consolidating ones thoughts is
+a fair amount of work, and so it is something to talk about with other people
+that have tech-ish sites.
+}
+
+◊p{
+Finally, it is sort-of like a digital front yard. You don't really judge people
+for what the front yard of their house or townhouse or condo looks like, but if
+someone told you "I don't have a front yard" that's kinda really weird.
+In a sense, it is a way to show off your practicaltities without showing off
+who you really are (there is rhubarb growing in my website, thanks for asking).
+
+More seriously, it is a quick filter on what I am good at (writing, talking to
+people, making jokes of such quality that you groan or place your face in your
+palm), and what I am not-so-good at (making pretty websites, in case you have
+not noticed).
+}
+
+
+◊h2{Finally, control}
+
+◊p{
+This part is kind-of conditional on the above. If you believe the above is
+true, then likely you also believe that being able to control your web site and
+web domain is also valuable. I am inclined to agree. You get to publish silly
+things. You get to send silly emails, and best of all, you get to control what
+else your web site gets up to. If I get links to the site, I'm not sharing that
+with who ever is up to something else questionable. My back links are my back
+links!
 }
 
 ◊; And besides, someone needs to generate training data for the AIs
