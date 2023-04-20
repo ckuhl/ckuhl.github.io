2135d5b6e97975cef1fcdd12db933b25d545ccc3
Chris Kuhl <git@ckuhl.com>
2023-04-19 19:16:35 -0600

    content: Add new article
    This is an article about writing style and all that.

---
+++ src/weblog/questions-for-the-reader.html.pm
@@ -0,0 +1,117 @@
+#lang pollen
+
+◊(define-meta title "Questions for the reader")
+◊(define-meta date "2023-04-19")
+
+
+◊(h1 (select-from-metas 'title (current-metas)))
+◊(div (em (select-from-metas 'date (current-metas))))
+
+
+◊(h2 "High School")
+◊p{
+When I was in high school, my assignments often had me reading books.
+
+These books would sometimes end with a section dedicated to "Questions for the reader."
+
+Rarely I would be tasked with answering these questions, but more often I would be left perplexed at who these questions were written for.
+
+In retrospect these were geared not at students but to selling books to teachers and school administrators desperately tatting together a curriculum with more demands than time.
+
+As a human that requires food and therefore money to live, I can respect the hustle.
+}
+
+◊p{
+There was a secondary impact on me, the young-and-malleable reader.
+
+You could never finish a book in high school without being smacked in the face with a "but why."
+
+Books did not end with a satisfying resolution of conflict, but with a set of questions that, regardless of your personal interest, you would lay awake thinking about.
+}
+
+
+◊(h2 "University")
+◊p{
+When I progressed on to university, I did not have to read as much fiction (whether or not I wanted to).
+
+I had to read a lot of textbooks, but I did not have to read things that had an implied question.
+
+Business textbooks, math textbooks, economics textbooks, computer science textbooks, they all ended the chapter with a very direct question (or questions).
+}
+
+◊p{
+Then it got worse.
+
+Third-, fourth-year courses started asking for literature reviews, where one would have to go and read papers, written by adults (now recognized as slightly older iterations on the same template I came from).
+
+Those papers all came with an interesting, common theme though:
+
+They would end with a paragraph on questions their research raised, without answering those questions.
+}
+
+◊p{
+This was not too far removed from the questions I was asked in high school.
+
+Yet the questions had a different impact on me.
+
+Instead of a large gulf between the writer and the reader, the distance was two, or even just one step (undergraduate to masters, or up again to doctorate).
+
+It was the realization that I was not too far removed from these people.
+
+If they could ask questions, then maybe it was that I could too.
+}
+
+
+◊(h2 "Now")
+◊p{
+As it turns out, keeping a web log is a lot of work.
+
+In a yet-to-be-written reflection on previous iterations of this very log, I will be sure to enumerate why this is so much work.
+
+For now you may rest easy knowing there are two main reasons that I write web log posts:
+
+◊ol{
+	◊li{Occasionally to capture some knowledge I could not find elsewhere}
+	◊li{More often, to consolidate a thought that I haven not yet coalesced into a concrete idea}
+}
+
+That is to say, to take some inchoate idea I want to people about, and iterate on it until I have a concrete idea I can share with others.
+
+I am not sure that I always can do that.
+
+I am limited and human and can not litigate every idea I have to the ends of the earth.
+}
+
+◊p{
+Yet, sometimes I still want to write about an idea that I think is neat.
+
+Other times, there may be some kernel I have not yet unearthed, but I have run out of people to talk to (or at) as I work through my thoughts.
+}
+
+
+◊(h2 "So what?")
+◊p{
+Instead of ending with a confident, thought-leader-like assertion, I feel that it is okay, and even desirable, to end my web log posts with a series of questions.
+
+Maybe you could jest I am perpetuating a weird cycle, forcing future readers to think with their brains.
+}
+
+
+◊(h2 "[Bonus]")
+◊p{
+Such questions serve a secondary purpose, one designed for the internet era of drive-by observation.
+
+When content poses a direct question, not answering that question can feel rude.
+
+Often when you see content shared on a content aggregator, be it Reddit or Hacker News, it can attract commentary on the site itself, or on the style of the writer, when the content itself is complex and difficult to critique.
+}
+
+◊p{
+Posing questions at the end of the article can be a way to distract an angry mob from attacking arbitrary and unrelated points.
+
+In essence, it creates a threshold: "Your comment must be this interesting to be posted," otherwise it will be drowned out by people answering the Questions For The Reader.
+}
+
+◊p{
+What do you think?
+}
