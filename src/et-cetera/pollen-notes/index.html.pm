18d95647eaabc0c7252b3d56198f3f371300dd03
Chris Kuhl <git@ckuhl.com>
2023-06-08 08:34:55 -0400

    refactor: Move Pollen Notes to subdirectory
    This moves Pollen Notes into a subdirectory of `et-cetera`, which feels
    like a better place organizationally to have them.
    
    From here, we can approach more refactoring in the future, but it works
    for now at least.

--- src/et-cetera/pollen-notes/index.html.pm
+++ src/et-cetera/pollen-notes/index.html.pm
@@ -7,11 +7,13 @@
  )
 
 ◊; TODO: Is this the best way to insert the current pagetree?
-◊(let () (current-pagetree (load-pagetree "../index.ptree")) "")
+◊(let () (current-pagetree (load-pagetree "../../index.ptree")) "")
 
 ◊(define-meta title "Notes on Pollen")
 
-
 ◊(h1 "Notes on Pollen")
 
-◊(apply ul (map (compose li node->link) (children 'notes-on-pollen/index.html)))
+◊(apply
+ ul
+ (map (compose li node->link)
+ (children 'et-cetera/pollen-notes/index.html)))
