f9cbf7a57ecad993a636ee78433506a9a04bd139
Chris Kuhl <git@ckuhl.com>
2023-10-22 18:54:39 -0600

    feat: Add words page initial version

---
+++ src/et-cetera/words.html.pm
@@ -0,0 +1,20 @@
+#lang pollen
+
+◊(require
+ pollen/pagetree
+ pollen/template/html
+ racket/list
+ "words.rkt"
+ )
+
+◊; TODO: Is this the best way to insert the current pagetree?
+◊(let () (current-pagetree (load-pagetree "../index.ptree")) "")
+
+◊(define-meta title "Words I had to look up")
+
+◊(h1 "Words I had to look up")
+
+◊(ul
+◊for/splice[[(word words)]]{
+	◊li{◊word}}
+)
